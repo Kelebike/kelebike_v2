b8a691901f36a2ddffd8bb57855c413f6d2cb422
Mert Gürer <mert.gurer@hotmail.com.tr>
2022-12-02 21:27:33 -0800

    welcome page done
    added color scene (primary and secondry colars)

---
+++ iOS/kelebike/kelebike/Feature/Authentication/Welcome/PageView.swift
@@ -0,0 +1,79 @@
+//
+//  PageView.swift
+//  kelebike
+//
+//  Created by Mert on 2.12.2022.
+//
+
+import UIKit
+
+// UI view that is the template of every welcome page
+class PageView: UIView {
+    
+    private weak var imageView: UIImageView!
+    private weak var descriptionTextView: UITextView!
+    
+    var imageName: String
+    var descriptionHead: String
+    var descriptionBody: String
+
+    init(imageName: String, descriptionHead: String, descriptionBody: String) {
+        self.imageName = imageName
+        self.descriptionHead = descriptionHead
+        self.descriptionBody = descriptionBody
+        
+        super.init(frame: CGRect())
+        
+        setup()
+        setupLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setup() {
+        // get image and load it
+        let imageView = UIImageView(image: UIImage(imageLiteralResourceName: imageName))
+        imageView.contentMode = .scaleAspectFill
+        //imageView.backgroundColor = .bgColor
+        
+        // set description text and style
+        let textView = UITextView()
+        let attributedText = NSMutableAttributedString(string: descriptionHead, attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 20)])
+        
+        attributedText.append(NSAttributedString(string: "\n\n\n" + descriptionBody, attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 15), NSAttributedString.Key.foregroundColor: UIColor.systemGray6 ]))
+        
+        textView.attributedText = attributedText
+        textView.textAlignment = .center
+        textView.isEditable = false
+        textView.isScrollEnabled = false
+        textView.backgroundColor = .bgColor
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        textView.translatesAutoresizingMaskIntoConstraints = false        
+
+        // add both image and text on the page
+        self.imageView = imageView
+        addSubview(self.imageView)
+        
+        self .descriptionTextView = textView
+        addSubview(self.descriptionTextView)
+    }
+    
+    func setupLayout(){
+        // constraints for the image
+        NSLayoutConstraint.activate([
+            imageView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            imageView.topAnchor.constraint(equalTo: topAnchor, constant: 100),
+            imageView.widthAnchor.constraint(equalToConstant: 300),
+            imageView.heightAnchor.constraint(equalToConstant: 300)])
+        
+        // constraints for the description text
+        NSLayoutConstraint.activate([
+            descriptionTextView.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 75),
+            descriptionTextView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 48),
+            descriptionTextView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -48),
+            descriptionTextView.bottomAnchor.constraint(equalTo: bottomAnchor)])
+    }
+}
---
+++ iOS/kelebike/kelebike/Feature/Authentication/Welcome/WelcomeViewController.swift
@@ -0,0 +1,191 @@
+//
+//  WelcomeViewController.swift
+//  kelebike
+//
+//  Created by Mert on 2.12.2022.
+//
+
+import UIKit
+
+extension UIColor {
+    // extension to UIColor for easy acces of background color
+    static var bgColor = UIColor(red: 201/255, green: 226/255, blue: 101/255, alpha: 1)
+    static var accentColor = UIColor(red: 29/255, green: 73/255, blue: 167/255, alpha: 1)
+}
+
+class WelcomeViewController: UIViewController {
+    
+// ======= start of creating UI components ======= //
+    
+    // bottom bar that holds the buttons and indicator bar
+    var bottomControlStackView = UIStackView()
+    
+    // previous button
+    private let previousButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Prev", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        button.setTitleColor(.accentColor, for: .normal)
+        button.addTarget(self, action: #selector(prevPressed), for: .touchUpInside)
+
+        return button
+    }()
+    
+    // next button
+    private let nextButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Next", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        button.setTitleColor(.accentColor, for: .normal)
+        button.addTarget(self, action: #selector(nextPressed), for: .touchUpInside)
+        
+        return button
+    }()
+    
+    // page indicator bar
+    lazy var pageControl: UIPageControl = {
+        let pc = UIPageControl()
+        pc.currentPage = 0
+        pc.numberOfPages = pages.count
+        pc.currentPageIndicatorTintColor = .accentColor
+        pc.pageIndicatorTintColor = .accentColor.withAlphaComponent(0.6)
+        pc.addTarget(self, action: #selector(pageControlTapHandler(sender:)), for: .touchUpInside)
+        
+        return pc
+    }()
+    
+    lazy var page0: PageView = {
+        let view = PageView(imageName: "bike_icon",
+                            descriptionHead: "Rent bikes with the KeleBike App",
+                            descriptionBody: "Welcome to GTU kelebike app. Sign-up with yout @gtu mail and start riding the school bikes in no time")
+        return view
+    }()
+    
+    lazy var page1: PageView = {
+        let view = PageView(imageName: "how_icon",
+                            descriptionHead: "how to rent",
+                            descriptionBody: "okey  ")
+        return view
+    }()
+    
+    lazy var page2: PageView = {
+        let view = PageView(imageName: "helmet_icon",
+                            descriptionHead: "safety",
+                            descriptionBody: "okey")
+        return view
+    }()
+    
+    lazy var page3: PageView = {
+        let view = PageView(imageName: "garage_icon",
+                            descriptionHead: "maintenance",
+                            descriptionBody: "okey  ")
+        return view
+    }()
+    
+// ======= end of creating UI components ======= //
+    
+    // array of all pages
+    lazy var pages = [page0, page1, page2, page3]
+    
+    // creating and putting all the pages in to a scroll view
+    lazy var scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.showsHorizontalScrollIndicator = false
+        scrollView.isPagingEnabled = true
+        scrollView.contentSize = CGSize(width: view.frame.width * CGFloat(pages.count), height: view.frame.height)
+        
+        // add pages to the scroll view in a for loop
+        for i in 0..<pages.count {
+            scrollView.addSubview(pages[i])
+            pages[i].frame = CGRect(x: view.frame.width * CGFloat(i), y: 0, width: view.frame.width, height: view.frame.height)
+        }
+        scrollView.delegate = self
+        return scrollView
+    }()
+    
+    // main view func
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .bgColor
+
+        setupComponents()
+        setupLayout()
+    }
+    
+    // add all the UI components to the view
+    func setupComponents() {
+        // create a stack view for the bottom conponents
+        bottomControlStackView = UIStackView(arrangedSubviews: [previousButton, pageControl, nextButton])
+        bottomControlStackView.translatesAutoresizingMaskIntoConstraints = false
+        bottomControlStackView.distribution = .fillEqually
+        
+        // add scrool view (all the pages)
+        view.addSubview(scrollView)
+        // add bottom bar
+        view.addSubview(bottomControlStackView)
+    }
+    
+    // place constrains for all the object in the view
+    func setupLayout() {
+        // constraints for scroll view
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor)])
+        
+        // constraints for the page control bar
+        bottomControlStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            bottomControlStackView.heightAnchor.constraint(equalToConstant: 50),
+            bottomControlStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            bottomControlStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            bottomControlStackView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -24)])
+    }
+    
+    // prev button functionality
+    @objc private func prevPressed() {
+        var frame: CGRect = scrollView.frame
+        frame.origin.x = frame.size.width * CGFloat(pageControl.currentPage - 1)
+        scrollView.scrollRectToVisible(frame, animated: true)
+    }
+    
+    // next button functionality
+    @objc private func nextPressed() {
+        var frame: CGRect = scrollView.frame
+        frame.origin.x = frame.size.width * CGFloat(pageControl.currentPage + 1)
+        scrollView.scrollRectToVisible(frame, animated: true)
+        
+        if(pageControl.currentPage == pages.count - 1) {
+            performSegue(withIdentifier: "endOfWelcome", sender: nil)
+        }
+    }
+    
+    // objective-c code that handles the bottom bar press
+    @objc func pageControlTapHandler(sender: UIPageControl){
+        var frame: CGRect = scrollView.frame
+        frame.origin.x = frame.size.width * CGFloat(sender.currentPage)
+        scrollView.scrollRectToVisible(frame, animated: true)
+    }
+}
+
+// scroll view delegate to track and update buttom bar index
+extension WelcomeViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let pageIndex = round(scrollView.contentOffset.x / view.frame.width)
+        pageControl.currentPage = Int(pageIndex)
+        
+        if (pageControl.currentPage == pages.count - 1) {
+            nextButton.setTitle("I understand", for: .normal)
+        }
+        else{
+            nextButton.setTitle("Next", for: .normal)
+        }
+    }
+}
+
+
